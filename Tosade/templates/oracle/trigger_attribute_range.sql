98a4478e44a4603a27a8886c8d110bf5a47cf9d0
Chaiba <kamikaazo@hotmail.com>
2015-01-23 21:07:24 +0100

    templates uitgelijnd

--- Tosade/templates/oracle/trigger_attribute_range.sql
+++ Tosade/templates/oracle/trigger_attribute_range.sql
@@ -1,13 +1,13 @@
 
-declare
-	l_passed boolean := true;
-begin
-	if l_oper in ({{triggerOperator}})
-	then
-		l_passed := {{triggerPassed}};
-		if not l_passed
-		then
-			l_error_stack := l_error_stack || '{{errorMessage}}';
-		end if;
-	end if;
-end;
+    declare
+            l_passed boolean := true;
+    begin
+        if l_oper in ({{triggerOperator}})
+        then
+            l_passed := {{triggerPassed}};
+            if not l_passed
+            then
+                l_error_stack := l_error_stack || '{{errorMessage}}';
+            end if;
+        end if;
+    end;
